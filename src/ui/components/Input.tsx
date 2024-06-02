8404d035d794709f1dca24118970e01f1084942c
John Saady <john.saady@gmail.com>
2024-06-02 11:39:28 -0600

    fix: handle default value for date picker

--- src/ui/components/Input.tsx
+++ src/ui/components/Input.tsx
@@ -5,9 +5,12 @@ export interface InputExtras {
 }
 
 export const Input = forwardRef<HTMLInputElement, React.HTMLProps<HTMLInputElement>&InputExtras>(({ ...props }, ref) => {
-  // if ('value' in props && props.value === null) {
-  //   props.value = '';
-  // }
+  if ('defaultValue' in props && props.type === 'date') {
+    // format the date
+    const date = new Date(props.defaultValue as string);
+    props.defaultValue = date.toISOString().split('T')[0];
+  }
+
   return <>
     {props.label && <label className=" invalid:text-red-500 block text-sm font-semibold leading-6 text-neutral-900 dark:text-white w-full mt-4" hidden={props.hidden}>{props.label}</label>}
     <input {...props} ref={ref} className={`invalid:ring-red-500 block w-full rounded-md border-0 px-3.5 py-2 text-neutral-900 shadow-sm ring-1 ring-inset ring-neutral-300 placeholder:text-neutral-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 dark:bg-black dark:text-white dark:ring-neutral-600 sm:text-sm sm:leading-6 ${props.className ?? ''} `} />
