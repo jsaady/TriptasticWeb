9575aa4146a9391f0254c25a3153d214c35ce6bb
John Saady <john.saady@gmail.com>
2024-06-02 11:32:35 -0600

    fix: refactor buttons to actually render colors

--- src/ui/components/Button.tsx
+++ src/ui/components/Button.tsx
@@ -1,19 +1,21 @@
 import { forwardRef } from 'react';
 
-export const Button = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
-  <button ref={ref} {...(props as any)} className={`${props.className ?? ''} rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-indigo-300`}>{props.children}</button>
-))
+const BaseButton = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
+  <button ref={ref} {...(props as any)} className={`${props.className ?? ''} rounded-md px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`}>{props.children}</button>
+));
 export const SmallButton = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
   <button ref={ref} {...(props as any)} className={`rounded-md bg-indigo-600 px-2.5 py-1.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${props.className ?? ''} disabled:bg-indigo-300`}>{props.children}</button>
-))
+));
 export const PrimaryButton = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
-  <Button ref={ref as any} {...props} className={`${props.className ?? ''} bg-blue-600 hover:bg-blue-500`}>{props.children}</Button>
+  <BaseButton ref={ref as any} {...props} className={`${props.className ?? ''} bg-indigo-600 hover:bg-indigo-500 focus-visible:outline-indigo-600 disabled:bg-indigo-300`}>{props.children}</BaseButton>
 ));
 
 export const SecondaryButton = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
-  <Button ref={ref as any} {...props} className={`${props.className ?? ''} bg-gray-600 hover:bg-gray-500`}>{props.children}</Button>
+  <BaseButton ref={ref as any} {...props} className={`${props.className ?? ''} bg-gray-600 hover:bg-gray-500 focus-visible:outline-gray-600 disabled:bg-gray-300`}>{props.children}</BaseButton>
 ));
 
 export const LinkButton = forwardRef((props: React.HTMLProps<HTMLButtonElement>, ref) => (
-  <Button ref={ref as any} {...props} className={`${props.className ?? ''} bg-transparent underline`}>{props.children}</Button>
+  <BaseButton ref={ref as any} {...props} className={`${props.className ?? ''} bg-transparent underline`}>{props.children}</BaseButton>
 ));
+
+export const Button = PrimaryButton;
